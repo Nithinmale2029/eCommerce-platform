1a9eb300a379010cce1d5439a19ad01e78371984
Nithinmale2029 <malenithin278@gmail.com>
2026-03-19 01:21:43 -0400

    Create ECommerceApplication.java

---
+++ src/main/java/com/microservice/discovery_service/ECommerceApplication.java
@@ -0,0 +1,13 @@
+package com.microservice.discovery_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ECommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ECommerceApplication.class, args);
+	}
+
+}
